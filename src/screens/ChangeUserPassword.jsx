09d3c9ced302651214c4d3d4ba722fd116990424
Andriana Ihnatyshch <andriana@pettersonapps.com>
2019-01-22 13:17:16 +0200

    not changes

--- src/screens/ChangeUserPassword.jsx
+++ src/screens/ChangeUserPassword.jsx
@@ -21,7 +21,8 @@ class ChangeUserPassword extends Component {
                 changePassword
             },
             state: {
-               newPass, repeatNewPass
+               newPass,
+               repeatNewPass
             }
         } = this;
 
